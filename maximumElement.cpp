2ff4fee7b91ec634719b31476de1d7f3ddcf6d85
jayram10125 <jayram.10125@gmail.com>
2025-01-02 23:20:25 +0530

    maximum element

---
+++ maximumElement.cpp
@@ -0,0 +1,26 @@
+//find the maximum elements out of all the elements
+#include<iostream>
+using namespace std;
+int main()
+{
+    int size;
+    cout<<"enter the size of array : ";
+    cin>>size;
+    int arr[size];
+    cout<<"enter the elements of the array  :";
+    for(int i=0;i<size;i++)
+    {
+        cin>>arr[i];
+    }
+    int max=arr[0];
+    for(int i=0;i<size;i++)
+    {
+        if(arr[0]<arr[i])
+        {
+            max=arr[i];
+        }
+    }
+    cout<<"the marimum element is : "<<max;
+    
+} 
+
