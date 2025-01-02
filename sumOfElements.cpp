e2a104802887c65bfad8861b0427c755ff1aa66f
jayram10125 <jayram.10125@gmail.com>
2025-01-02 23:37:17 +0530

    sum of all the elements

--- sumOfElements.cpp
+++ sumOfElements.cpp
@@ -5,5 +5,18 @@ int main()
 {
     int n;
     cout<<"enter the size of array : ";
-    
+    cin>>n;
+    int sum=0;
+    int arr[n];
+    cout<<"enter the elements of array : ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+    }
+    cout<<"sum of all the elements of array is : "<<sum;
+    return 0;
 }
